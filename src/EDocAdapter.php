8e4df0e0bc055cbb5eac868324c07250f73fdd0e
Mikkel Ricky <rimi@aarhus.dk>
2018-10-15 10:24:45 +0200

    HRIN-140: Initial commit

---
+++ src/EDocAdapter.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of itk-dev/flysystem-edoc.
+ *
+ * (c) 2018 ITK Development
+ *
+ * This source file is subject to the MIT license.
+ */
+
+namespace ItkDev\FlysystemEDoc;
+
+use League\Flysystem\Adapter\AbstractAdapter;
+
+/**
+ * Flysystem eDoc Adapter.
+ */
+class EDocAdapter extends AbstractAdapter
+{
+}
